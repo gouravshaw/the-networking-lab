cb404ba1df6bd7f0eeb6122999b9c852ed4dc1d7
gouravshaw <iamgshaw@gmail.com>
2026-02-15 21:25:08 +0000

    added the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { UserProvider, useUser } from './context/UserContext';
+import { UserProvider } from './context/UserContext';
 import { ThemeProvider } from './context/ThemeContext';
 import TopNav from './components/TopNav';
 import Home from './pages/Home';
 import Session from './pages/Session';
-import Setup from './pages/Setup';
 import IntroBuilder from './pages/IntroBuilder';
 import Toolbox from './pages/Toolbox';
 import Contacts from './pages/Contacts';
 import ContactDetail from './pages/ContactDetail';
 
 function AppRoutes() {
-      const { hasCompletedSetup } = useUser();
-
-      if (!hasCompletedSetup) {
-            return (
-                  <Routes>
-                        <Route path="/setup" element={<Setup />} />
-                        <Route path="*" element={<Navigate to="/setup" replace />} />
-                  </Routes>
-            );
-      }
-
       return (
             <>
                   <Routes>
@@ -32,7 +20,6 @@ function AppRoutes() {
                         <Route path="/toolbox" element={<Toolbox />} />
                         <Route path="/contacts" element={<Contacts />} />
                         <Route path="/contacts/:id" element={<ContactDetail />} />
-                        <Route path="/setup" element={<Navigate to="/" replace />} />
                         <Route path="*" element={<Navigate to="/" replace />} />
                   </Routes>
                   <TopNav />
